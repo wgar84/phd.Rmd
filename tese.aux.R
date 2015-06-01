3e8cb115c4f169e1e948e0dbaabf6e50dd4f469f
Guilherme Garcia <pectinidae@gmail.com>
2015-06-01 16:41:24 -0300

    begin

---
+++ tese.aux.R
@@ -0,0 +1,68 @@
+require (geomorph)
+require (shapes)
+require (RColorBrewer)
+require (ape)
+require (evolqg)
+require (expm)
+require (plyr)
+require (plotrix)
+require (doMC)
+require (reshape2)
+require (ggplot2)
+require (phytools)
+require (geiger)
+require (mvtnorm)
+require (MCMCglmm)
+require (grid)
+require (gridExtra)
+require (gridBase)
+require (surface)
+require (rmarkdown)
+require (knitr)
+require (pander)
+require (shape)
+require (adephylo)
+require (dplyr)
+require (magrittr)
+require (tidyr)
+
+registerDoMC (cores = 10)
+
+## require (devtools)
+## install_github('uyedaj/bayou')
+## install_github ('mkoohafkan/kfigr')
+
+attach ('../Databases/Reference.RData')
+attach ('../Databases/ED.RData')
+attach ('../Databases/Sym.RData')
+attach ('../Databases/OneDef.RData')
+attach ('../Databases/Tree.RData')
+attach ('../Databases/Aux.RData')
+attach ('../Databases/LifeHistory.RData')
+
+options(contrasts = c('contr.sum', 'contr.poly'))
+
+.source.files <- dir('Func', pattern = '.R', full.names = TRUE)
+.source.files <- .source.files [!grepl ('~', .source.files)]
+for (i in 1:length (.source.files))
+  source (.source.files [i])
+
+.source.files <- dir('../Allo/FuncR/', pattern = '.R', full.names = TRUE)
+.source.files <- .source.files [!grepl ('~', .source.files)]
+for (i in 1:length (.source.files))
+  source (.source.files [i])
+
+.source.files <- dir('../covTensor/Func/', pattern = '.R', full.names = TRUE)
+.source.files <- .source.files [!grepl ('~', .source.files)]
+for (i in 1:length (.source.files))
+  source (.source.files [i])
+
+attach ('Data/modcomp.Results.RData')
+attach ('Data/allo.Results.RData')
+attach ('Data/ppca.RData')
+
+render ('tese.Rmd')
+system ('okular tese.pdf &')
+
+render('allo.Rmd')
+system ('okular allo.pdf &')
